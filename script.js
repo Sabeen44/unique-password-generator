fc00e514924bf423f9b80cb3e52778334c5530db
Beenz <CH.SABEEN@GMAIL.COM>
2023-03-30 22:18:01 -0700

    added eventlistener to show passowrd on click

--- script.js
+++ script.js
@@ -15,79 +15,93 @@
 //choose randomly from the lists according to promt answers
 //generate password
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
 var pwLength;
 
 var passwordKey = {
   lowerCase: "abcdefghijklmnopqrstuvwxyz",
   upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numbers: "0123456789",
-  symbols: "!#$%&'",
+  symbols: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
 };
 
 var buildPassword = "";
 
-// Write password to the #password input
+function promptFunction() {
+  pwLength = prompt("choose length of password (between 8 to 128 Characters)");
+}
+
+// Prompts for user to request their criteria
 function writePassword() {
   var pwNum = confirm("Do you want to include Numbers?");
   var pwUpper = confirm("Do you want to include UpperCase Letters?");
   var pwLower = confirm("Do you want to include LowerCase Letters?");
   var pwSymbol = confirm("Do you want to include special characters?");
-  var password = generatePassword();
 
+  //while the password length is not reched, continue running the switch cases
+  while (buildPassword.length < pwLength) {
+    var random = Math.floor(Math.random() * 4);
 
-while (buildPassword.length < pwLength) {
-    var selection = Math.floor(Math.random() * 4);
-  switch (selection) {
-    case 0:
-      if (pwNum === true) {
-        var secureNum =
-          passwordKey.numbers[
-            Math.floor(Math.random() * passwordKey.numbers.length)
-          ];
+    switch (random) {
+      case 0:
+        if (pwNum === true) {
+          var secureNum =
+            passwordKey.numbers[
+              Math.floor(Math.random() * passwordKey.numbers.length)
+            ];
 
-        buildPassword = buildPassword + secureNum;
-      }
-      break;
-    case 1:
-      if (pwUpper === true) {
-        var secureUpChar =
-          passwordKey.upperCase[
-            Math.floor(Math.random() * passwordKey.upperCase.length)
-          ];
-        buildPassword = buildPassword + secureUpChar;
-      }
-      break;
+          buildPassword = buildPassword + secureNum;
+        }
+        break;
+      case 1:
+        if (pwUpper === true) {
+          var secureUpChar =
+            passwordKey.upperCase[
+              Math.floor(Math.random() * passwordKey.upperCase.length)
+            ];
+          buildPassword = buildPassword + secureUpChar;
+        }
+        break;
 
-    case 2:
-      if (pwLower === true) {
-        var secureLowChar =
-          passwordKey.lowerCase[
-            Math.floor(Math.random() * passwordKey.lowerCase.length)
-          ];
-        buildPassword = buildPassword + secureLowChar;
-      }
-      break;
-    case 3:
-      if (pwSymbol === true) {
-        var secureSymbol =
-          passwordKey.symbols[
-            Math.floor(Math.random() * passwordKey.symbols.length)
-          ];
-        buildPassword = buildPassword + secureSymbol;
-      }
-      break;
+      case 2:
+        if (pwLower === true) {
+          var secureLowChar =
+            passwordKey.lowerCase[
+              Math.floor(Math.random() * passwordKey.lowerCase.length)
+            ];
+          buildPassword = buildPassword + secureLowChar;
+        }
+        break;
+      case 3:
+        if (pwSymbol === true) {
+          var secureSymbol =
+            passwordKey.symbols[
+              Math.floor(Math.random() * passwordKey.symbols.length)
+            ];
+          buildPassword = buildPassword + secureSymbol;
+        }
+        break;
 
-    default:
-      alert("Please pick atleast one option");
+      default:
+      //alert("Please pick atleast one option");
+    }
+    console.log(buildPassword);
   }
+  //output the password in the textarea
+  var passwordText = (document.querySelector("#password").value =
+    buildPassword);
+}
 
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+//to set condtion for the password length that can be generated
+function showPassword() {
+  promptFunction();
+  if (pwLength < 8 || pwLength > 128) {
+    alert("password can only be 8-128 characters long");
+    return;
+  } else {
+    writePassword();
+  }
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", showPassword);
